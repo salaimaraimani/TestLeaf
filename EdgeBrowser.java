8c8df22932be58ab01fb6b1089e47acf4d6b9bdf
salaimaraimani <101454719+salaimaraimani@users.noreply.github.com>
2023-12-21 19:01:27 +0530

    Delete EdgeBrowser.java

--- EdgeBrowser.java
+++
@@ -1,9 +0,0 @@
-package week1.day2;
-
-public class EdgeBrowser {
-	public static void main(String[] args) {
-		Browser test = new Browser();
-		test.launchBrowser("chrome");
-		test.loadUrl();
-	}
-}
