a103c9651a36399aba49a0303f3b2034f982d718
salaimaraimani <101454719+salaimaraimani@users.noreply.github.com>
2023-12-21 19:00:53 +0530

    Add files via upload

---
+++ week1/day2/EdgeBrowser.java
@@ -0,0 +1,9 @@
+package week1.day2;
+
+public class EdgeBrowser {
+	public static void main(String[] args) {
+		Browser test = new Browser();
+		test.launchBrowser("chrome");
+		test.loadUrl();
+	}
+}
