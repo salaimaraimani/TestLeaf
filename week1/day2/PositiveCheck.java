5d2661f2d3b79ec2d668f26d88a36f502a4f3637
salaimaraimani <101454719+salaimaraimani@users.noreply.github.com>
2023-12-25 10:33:28 +0530

    Add files via upload

---
+++ week1/day2/PositiveCheck.java
@@ -0,0 +1,18 @@
+package week1.day2;
+
+public class PositiveCheck {
+
+	public static void main(String[] args) {
+		int number = 35;
+
+		if (number < 0) {
+			System.out.println("The number is negative");
+		} else if (number >= 0) {
+			System.out.println("The number is Positive");
+		} else {
+			System.out.println("The number is neither positive nor negative");
+		}
+
+	}
+
+}
---
+++ week1/day2/PositiveConversion.java
@@ -0,0 +1,14 @@
+package week1.day2;
+
+public class PositiveConversion {
+	public static void main(String[] args) {
+		int number =-25;
+		
+		if (number<0) {
+			number =-number;
+			System.out.println("The number -"+number+" is converted to"+number);
+		}
+	}
+	
+
+}
